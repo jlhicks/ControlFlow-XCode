39778352f03af53d40de81815b4dc3acad06b7f7
James Hicks <jlh.zorch@gmail.com>
2015-07-23 19:49:46 -0700

    Add actual code.

--- ControlFlow-XCode/ViewController.swift
+++ ControlFlow-XCode/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-  }
+    let truckSpeed = 45
+    let lamboSpeed = 120
+    var mySpeed:Int
+    
+    mySpeed = 55
+    
+    if mySpeed < 70 {
+      println("Keep cruisin'!")
+    } else {
+      println("Slow down, police ahead!")
+    }
 
+    println("Hello world!")
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
